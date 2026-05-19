66168eb7b2eb9f98e20c3c5abdba70467a3e1628
nicolas-bourbaki03 <marysxxs@gmail.com>
2026-05-19 09:09:49 +0200

    scaffolds - report and dimers scripts

---
+++ scripts/dimers.py
@@ -0,0 +1,21 @@
+# dimers
+
+def reverse_complement(seq):
+    """
+    Returns reverse complement of a DNA sequence.
+    """
+    pass
+
+def self_complementarity(seq):
+    """
+    Checks if primer can form hairpin with itself.
+    Returns True if problematic.
+    """
+    pass
+
+def dimer_check(seq1, seq2):
+    """
+    Checks if two primers (forward + reverse) can bind to each other.
+    Returns True if problematic.
+    """
+    pass
--- scripts/report.py
+++ scripts/report.py
@@ -1,1 +1,24 @@
 # report
+
+import matplotlib.pyplot as plt
+import csv
+
+def save_csv(results, output_path):
+    """
+    Saves analysis results to a CSV file.
+    results: list of dicts {name, sequence, tm, gc, length, flags}
+    output_path: str, path to output CSV file
+    """
+    pass
+
+def plot_gc_tm(results, output_path):
+    """
+    Scatter plot: Tm vs GC content per primer.
+    """
+    pass
+
+def plot_flags(results, output_path):
+    """
+    Bar chart: number of flagged issues per primer.
+    """
+    pass
